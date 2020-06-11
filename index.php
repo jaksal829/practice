2dc30744ac5382a0c3fe934a48638c01cced003c
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-11 17:41:28 +0900

    Update index.php

--- index.php
+++ index.php
@@ -63,6 +63,12 @@
     .nav-item{
       padding: 15px;
       cursor: pointer;
+      align: left;
+    }
+    .nav-item1{
+      padding: 15px;
+      cursor: pointer;
+      align: right;
     }
     .nav-item a{
       text-align: center;
@@ -81,16 +87,6 @@
       text-align : center;
       border : 1px solid black;
     }
-    .area {
-      position: absolute;
-      background: #fff;
-      border: 1px solid #888;
-      border-radius: 3px;
-      font-size: 12px;
-      top: -5px;
-      left: 15px;
-      padding:2px;
-    }
     </style>
     <title>COVID-19</title>
    
@@ -98,10 +94,8 @@
 <body>
 
     <ul id="nav-ul"class="nav-container">
-      <li class="nav-item"><a href="">이재안</a></li>
-      <li class="nav-item"><a href="">이재건</a></li>
-      <li class="nav-item"><a href="">이희수</a></li>
-      <li class="nav-item"><a href="">신재원</a></li>
+      <li class="nav-item"><a style="text_align:left">코로나 맵</a></li>
+      <li class="nav-item1"><a style="text_align:right">기준</a></li>
     </ul>
   </nav>
 <!-- 지도를 표시할 div 입니다 -->
@@ -110,7 +104,7 @@
       <div id="map" class="container"></div>
   </ul>
   <ul class="cmap">
-      <iframe longdesc="covid simulation" width="1024" height="768" frameborder="0" scrolling="no" name="NeBoard" onLoad="ResizeFrame('NeBoard');" id="img" src="https://unity-technologies.github.io/unitysimulation-coronavirus-example-player/"></iframe>
+      <iframe longdesc="covid simulation" width="1024" height="668" frameborder="0" scrolling="no" name="NeBoard" onLoad="ResizeFrame('NeBoard');" id="img" src="https://unity-technologies.github.io/unitysimulation-coronavirus-example-player/"></iframe>
   </ul>
   <ul>
     <div class="flourish-embed flourish-bar-chart-race" data-src="visualisation/2675613" data-url="https://flo.uri.sh/visualisation/2675613/embed"><script src="https://public.flourish.studio/resources/embed.js"></script></div>
@@ -147,8 +141,8 @@
 <script>
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
-        center: new kakao.maps.LatLng(35.179791, 129.074975), // 지도의 중심좌표
-        level: 9 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(36.189320, 128.003166), // 지도의 중심좌표
+        level: 13 // 지도의 확대 레벨
         
     };
 var markerPosition  = {
@@ -160,8 +154,120 @@ var marker = new kakao.maps.Marker({
     position: markerPosition
 });
 
-map.setDraggable(false);
-map.setZoomable(false);
+//map.setDraggable(false);
+//map.setZoomable(false);
+$.getJSON("TL_SCCO_CTPRVN.json", function(geojson) {
+ 
+ var data = geojson.features;
+ var coordinates = [];    //좌표 저장할 배열
+ var name = '';            //행정 구 이름
+
+ $.each(data, function(index, val) {
+
+     coordinates = val.geometry.coordinates;
+     name = val.properties.SIG_KOR_NM;
+     
+     displayArea(coordinates, name);
+
+ })
+})
+
+
+var polygons=[];                //function 안 쪽에 지역변수로 넣으니깐 폴리곤 하나 생성할 때마다 배열이 비어서 클릭했을 때 전체를 못 없애줌.  그래서 전역변수로 만듦.
+ 
+//행정구역 폴리곤
+function displayArea(coordinates, name) {
+
+ var path = [];            //폴리곤 그려줄 path
+ var points = [];        //중심좌표 구하기 위한 지역구 좌표들
+ 
+ $.each(coordinates[0], function(index, coordinate) {        //console.log(coordinates)를 확인해보면 보면 [0]번째에 배열이 주로 저장이 됨.  그래서 [0]번째 배열에서 꺼내줌.
+     var point = new Object(); 
+     point.x = coordinate[1];
+     point.y = coordinate[0];
+     points.push(point);
+     path.push(new daum.maps.LatLng(coordinate[1], coordinate[0]));            //new daum.maps.LatLng가 없으면 인식을 못해서 path 배열에 추가
+ })
+ 
+ // 다각형을 생성합니다 
+ var polygon = new daum.maps.Polygon({
+     map : map, // 다각형을 표시할 지도 객체
+     path : path,
+     strokeWeight : 2,
+     strokeColor : '#004c80',
+     strokeOpacity : 0.8,
+     fillColor : '#fff',
+     fillOpacity : 0.7
+ });
+ 
+ polygons.push(polygon);            //폴리곤 제거하기 위한 배열
+
+ // 다각형에 mouseover 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 변경합니다 
+ // 지역명을 표시하는 커스텀오버레이를 지도위에 표시합니다
+ daum.maps.event.addListener(polygon, 'mouseover', function(mouseEvent) {
+     polygon.setOptions({
+         fillColor : '#09f'
+     });
+
+     customOverlay.setContent('<div class="area">' + name + '</div>');
+
+     customOverlay.setPosition(mouseEvent.latLng);
+     customOverlay.setMap(map);
+ });
+
+ // 다각형에 mousemove 이벤트를 등록하고 이벤트가 발생하면 커스텀 오버레이의 위치를 변경합니다 
+ daum.maps.event.addListener(polygon, 'mousemove', function(mouseEvent) {
+
+     customOverlay.setPosition(mouseEvent.latLng);
+ });
+
+ // 다각형에 mouseout 이벤트를 등록하고 이벤트가 발생하면 폴리곤의 채움색을 원래색으로 변경합니다
+ // 커스텀 오버레이를 지도에서 제거합니다 
+ daum.maps.event.addListener(polygon, 'mouseout', function() {
+     polygon.setOptions({
+         fillColor : '#fff'
+     });
+     customOverlay.setMap(null);
+ });
+
+ // 다각형에 click 이벤트를 등록하고 이벤트가 발생하면 해당 지역 확대을 확대합니다.
+ daum.maps.event.addListener(polygon, 'click', function() {
+     
+     // 현재 지도 레벨에서 2레벨 확대한 레벨
+     var level = map.getLevel()-2;
+     
+     // 지도를 클릭된 폴리곤의 중앙 위치를 기준으로 확대합니다
+     map.setLevel(level, {anchor: centroid(points), animate: {
+         duration: 350            //확대 애니메이션 시간
+     }});            
+
+     deletePolygon(polygons);                    //폴리곤 제거      
+ });
+}
+
+function centroid (points) {
+    var i, j, len, p1, p2, f, area, x, y;
+ 
+    area = x = y = 0;
+ 
+    for (i = 0, len = points.length, j = len - 1; i < len; j = i++) {
+            p1 = points[i];
+            p2 = points[j];
+ 
+            f = p1.y * p2.x - p2.y * p1.x;
+            x += (p1.x + p2.x) * f;
+            y += (p1.y + p2.y) * f;
+            area += f * 3;
+    }
+    return new daum.maps.LatLng(x / area, y / area);
+}
+function deletePolygon(polygons) {
+    for (var i = 0; i < polygons.length; i++) {
+        polygons[i].setMap(null);
+    }
+    polygons = [];
+}
+
 </script>
 </body>
 </html>
