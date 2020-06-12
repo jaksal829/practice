c59c2d88edd230492941159875cb4190a4a778f3
jaksal829 <57984456+jaksal829@users.noreply.github.com>
2020-06-12 13:21:37 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8023,28 +8023,11 @@ function displayArea(area) {
     kakao.maps.event.addListener(polygon, 'click', function(mouseEvent) {
       var level = map.getLevel()-2;
       
-      map.setLevel(level, {anchor: centroid(points), animate: {
-            duration: 350            //확대 애니메이션 시간
-      }}); 
+      map.setLevel(level);
+
       displayLevel();
       });
     
-    /*function centroid (points) {
-    var i, j, len, p1, p2, f, area, x, y;
- 
-    area = x = y = 0;
- 
-    for (i = 0, len = points.length, j = len - 1; i < len; j = i++) {
-            p1 = points[i];
-            p2 = points[j];
- 
-            f = p1.y * p2.x - p2.y * p1.x;
-            x += (p1.x + p2.x) * f;
-            y += (p1.y + p2.y) * f;
-            area += f * 3;
-    }
-    return new kakao.maps.LatLng(x / area, y / area);
-    }*/
 }
 
 map.setDraggable(false);
